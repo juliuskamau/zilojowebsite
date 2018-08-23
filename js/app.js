e8c3de9bebcb3384dd6a5b207d48b19746bd81de
julius kamau <julius.kamau@zilojo.com>
2018-08-23 20:47:34 +0300

    layou changes

--- js/app.js
+++ js/app.js
@@ -101,7 +101,7 @@ markdown.forEach(function(m){
 ***************************** */
 
 // Enable Smooth Scrolling ...  by Chris Coyier of CSS-Tricks.com
-$('a[href*="#"]:not([href="#"]):not([href="#show"]):not([href="#hide"])').click(function() {
+/*$('a[href*="#"]:not([href="#"]):not([href="#show"]):not([href="#hide"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
         var target = $(this.hash);
         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
@@ -115,34 +115,52 @@ $('a[href*="#"]:not([href="#"]):not([href="#show"]):not([href="#hide"])').click(
 });
 
 
+*/
+
+var bLazy = new Blazy({
+    breakpoints: [{
+    width: 420 // Max-width
+      , src: 'data-src-small'
+}]
+  , success: function(element){
+    setTimeout(function(){
+    // We want to remove the loader gif now.
+    // First we find the parent container
+    // then we remove the "loading" class which holds the loader image
+    var parent = element.parentNode;
+    parent.className = parent.className.replace(/\bloading\b/,'');
+    }, 200);
+    }
+});
 
 
-$(document).ready(function() {
-    var owl = $('.owl-carousel');
-    owl.owlCarousel({
-      autoplay: true,
-      items: 1,
-      nav: true,
-      loop: true,
-      autoplayHoverPause: true,
-      animateOut: 'slideOutUp',
-      animateIn: 'slideInUp',
-      autoplayTimeout: 5000,
-      autoplayHoverPause: false,
-    });
-    $('.play').on('click', function() {
-    owl.trigger('play.owl.autoplay', [1000]);
-      console.log('play');
-  })
-  $('.stop').on('click', function() {
-    owl.trigger('stop.owl.autoplay');
-         console.log('stop');
-  });
-  });
-  
+$('.owl-carousel').owlCarousel({
+//lazyLoad:true,
+autoplay: true,
+items:1,
+nav: true,
+loop: true,
+autoplayHoverPause: true,
+animateOut: 'slideOutUp',
+animateIn: 'slideInUp',
+dots: true,
+autoplayTimeout: 5000
+
+});
+
+
+
+
+
+objectFitImages();
+jarallax(document.querySelectorAll('.jarallax'));
+jarallax(document.querySelectorAll('.jarallax-keep-img'), {
+  keepImg: true,
+});
+
+
 
 
 
 $(document).foundation()
-
 
